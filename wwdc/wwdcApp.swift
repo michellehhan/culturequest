f94aa6f49a88abfb6b06e23ded8f4cf3d1f8be42
Michelle Han <michellehan2007agt@gmail.com>
2024-02-22 00:55:25 -0800

    Initial Commit

---
+++ wwdc/wwdcApp.swift
@@ -0,0 +1,17 @@
+//
+//  wwdcApp.swift
+//  wwdc
+//
+//  Created by Michelle Han on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct wwdcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
